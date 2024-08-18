30fed131f6af26191f20424ee10211add7c57a8d
Bendegúz Török <bendi.torok@gmail.com>
2024-08-18 08:38:32 +0200

    Export code from main

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,8 @@ use windows::{
     Win32::UI::WindowsAndMessaging::SW_SHOW,
 };
 
-pub fn request_admin_privileges() {
+#[cfg(target_os = "windows")]
+pub fn request_admin_privileges_windows() {
     // Check if the program is running with admin rights
     let is_admin = Command::new("net")
         .arg("session")
@@ -54,5 +55,64 @@ pub fn request_admin_privileges() {
 
         // Exit the current process as it will be relaunched
         std::process::exit(0);
+    }
+}
+
+pub fn run_windows() {
+    request_admin_privileges_windows();
+
+    println!("Running with elevated privileges!");
+
+    let gpu_name = std::env::args().nth(1);
+    //let gpu_name = Option::from(String::from("NVIDIA GeForce RTX 3050"));
+
+    if gpu_name.is_none() {
+        println!("No GPU name provided");
+        println!("Press enter to exit");
+
+        std::io::stdin().read_line(&mut String::new()).unwrap();
+        std::process::exit(0);
+    }
+
+    let gpu_name = gpu_name.unwrap();
+    println!("GPU name: {}", gpu_name);
+
+    loop {
+        let get_power_command = Command::new("powershell")
+            .arg("-Command")
+            .arg(r#"(Get-WmiObject -Class BatteryStatus -Namespace "root\wmi").PowerOnline"#)
+            .output()
+            .expect("Failed to execute command");
+
+        // Convert the output bytes to a string
+        let power_connected = std::str::from_utf8(&get_power_command.stdout)
+            .expect("Failed to convert output to string")
+            .trim();
+
+        // Print the parsed output
+        println!("power_connected: {}", power_connected);
+
+        //let power_status = power_status.stdout == [1] as [u8; 1];
+        match power_connected {
+            "False" => {
+                Command::new("powershell")
+                .arg("-Command")
+                .arg(format!("Get-PnpDevice | Where-Object {{ $_.FriendlyName -like \"{}\" }} | Disable-PnpDevice -Confirm:$false", gpu_name))
+                .spawn()
+                .expect("Failed to execute command");
+            }
+            "True" => {
+                Command::new("powershell")
+                    .arg("-Command")
+                    // Use this line instead of the one below Get-PnpDevice | Where-Object { $_.FriendlyName -like "*$gpuName*" } | Enable-PnpDevice -Confirm:$false}
+                    // where gpuName is gpu_name from above
+                    .arg(format!("Get-PnpDevice | Where-Object {{ $_.FriendlyName -like \"{}\" }} | Enable-PnpDevice -Confirm:$false", gpu_name))
+                    .spawn()
+                    .expect("Failed to execute command");
+            }
+            _ => println!("Invalid!"),
+        }
+
+        std::thread::sleep(std::time::Duration::from_secs(10));
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,66 +1,10 @@
-use std::{process::Command, time::Duration};
-
 fn main() {
     #[cfg(target_os = "windows")]
-    ddgpu::request_admin_privileges();
+    ddgpu::run_windows();
 
     #[cfg(not(target_os = "windows"))]
     {
         println!("Application only runs on windwos currently!");
         std::process::exit(0);
-    }
-
-    println!("Running with elevated privileges!");
-
-    let gpu_name = std::env::args().nth(1);
-    //let gpu_name = Option::from(String::from("NVIDIA GeForce RTX 3050"));
-
-    if gpu_name.is_none() {
-        println!("No GPU name provided");
-        println!("Press enter to exit");
-
-        std::io::stdin().read_line(&mut String::new()).unwrap();
-        std::process::exit(0);
-    }
-
-    let gpu_name = gpu_name.unwrap();
-
-    loop {
-        let get_power_command = Command::new("powershell")
-            .arg("-Command")
-            .arg(r#"(Get-WmiObject -Class BatteryStatus -Namespace "root\wmi").PowerOnline"#)
-            .output()
-            .expect("Failed to execute command");
-
-        // Convert the output bytes to a string
-        let power_connected = std::str::from_utf8(&get_power_command.stdout)
-            .expect("Failed to convert output to string")
-            .trim();
-
-        // Print the parsed output
-        println!("power_connected: {}", power_connected);
-
-        //let power_status = power_status.stdout == [1] as [u8; 1];
-        match power_connected {
-            "False" => {
-                Command::new("powershell")
-                .arg("-Command")
-                .arg(format!("Get-PnpDevice | Where-Object {{ $_.FriendlyName -like \"*{}*\" }} | Disable-PnpDevice -Confirm:$false", gpu_name))
-                .spawn()
-                .expect("Failed to execute command");
-            }
-            "True" => {
-                Command::new("powershell")
-                    .arg("-Command")
-                    // Use this line instead of the one below Get-PnpDevice | Where-Object { $_.FriendlyName -like "*$gpuName*" } | Enable-PnpDevice -Confirm:$false}
-                    // where gpuName is gpu_name from above
-                    .arg(format!("Get-PnpDevice | Where-Object {{ $_.FriendlyName -like \"*{}*\" }} | Enable-PnpDevice -Confirm:$false", gpu_name))
-                    .spawn()
-                    .expect("Failed to execute command");
-            }
-            _ => println!("Invalid!"),
-        }
-
-        std::thread::sleep(std::time::Duration::from_secs(10));
     }
 }
