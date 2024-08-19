7929661c6895965e666dcf321d8e9bafc72df6b2
Bendegúz Török <bendi.torok@gmail.com>
2024-08-19 21:14:36 +0200

    Add service scripts

---
+++ scripts/windows/CreateService.ps1
@@ -0,0 +1,16 @@
+# Define variables
+$serviceName = "ddgpu"
+$executablePath = "C:\services\ddgpu\ddgpu.exe"
+$arguments = "--name NVIDIA GeForce RTX 3050 Laptop GPU --hide false"
+
+# Define service description
+$description = "Disable dedicated GPU service. Runs as admin."
+
+# Install the service using New-Service
+New-Service -Name $serviceName -BinaryPathName "$executablePath $arguments" -Description $description -DisplayName "My Custom Service" -StartupType Automatic
+
+# Set the service to run as LocalSystem
+sc.exe config $serviceName obj= LocalSystem
+
+# Start the service
+Start-Service -Name $serviceName
---
+++ scripts/windows/DeleteService.ps1
@@ -0,0 +1,5 @@
+# Define the service name
+$serviceName = "ddgpu"
+
+# Delete the service
+sc.exe delete $serviceName
